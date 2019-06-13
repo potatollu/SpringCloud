9ff742dc547b2092d6e7d3cb97ccd6c9d1c4c001
lilu <lilu@ly-sky.com>
2019-06-13 17:04:51 +0800

    Hystrix Dashboard

--- service-client/src/main/java/com/lilu/serviceclient/ServiceClientsApplication.java
+++ service-client/src/main/java/com/lilu/serviceclient/ServiceClientsApplication.java
@@ -1,16 +1,65 @@
 package com.lilu.serviceclient;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 //http://localhost:9002/test/hello
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
+@RestController
+@EnableHystrix
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 public class ServiceClientsApplication {
+
+	/**
+	 * 访问地址 http://localhost:9002/actuator/hystrix.stream
+	 * @param args
+	 */
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceClientsApplication.class, args);
 	}
 
+	@Value("${server.port}")
+	String port;
+
+	@RequestMapping("hello")
+	@HystrixCommand(fallbackMethod = "hiError")
+	public String home(@RequestParam(value = "name", defaultValue = "forezp")String name){
+		return "Hi" + name + ", I am from port: " + port;
+	}
+
+	public String hiError(String name) {
+		return "Hi, " + name + ",soory,error!";
+	}
+
+	/**
+	 * 解决断路器仪表盘404
+	 * @return
+	 */
+	@Bean
+	public ServletRegistrationBean getServlet() {
+		HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+		ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+		registrationBean.setLoadOnStartup(1);
+		// http://localhost:8765/actuator/hystrix.stream加不加actuator下面这句说的算
+		registrationBean.addUrlMappings("/actuator/hystrix.stream");
+		registrationBean.setName("HystrixMetricsStreamServlet");
+		return registrationBean;
+	}
 }
